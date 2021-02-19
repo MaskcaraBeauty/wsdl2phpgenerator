69b06873b8d54af5fee63d46ec4d86887c7678eb
Brance Boren <branceboren@gmail.com>
2021-02-19 15:57:18 -0700

    Parenthesized nested ternary. This makes it compatible with PHP 8.x

--- src/ArrayType.php
+++ src/ArrayType.php
@@ -237,7 +237,7 @@ class ArrayType extends ComplexType
                 false,
                 false
             ),
-            '  return is_countable($this->' . $this->field->getName() . ') ? count($this->' . $this->field->getName() . ') : $this->' . $this->field->getName() . ' ? 1 : 0;',
+            '  return is_countable($this->' . $this->field->getName() . ') ? count($this->' . $this->field->getName() . ') : ($this->' . $this->field->getName() . ' ? 1 : 0);',
             $countDock
         );
         $this->class->addFunction($count);
